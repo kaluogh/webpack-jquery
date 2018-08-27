f021a089f78517ced653f7695cf5fdf1f8e85423
kaluo <zhangwenxin6615006@163.com>
2018-08-27 11:41:32 +0800

    add bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,5 @@ require('commonCss');
 require('../css/index.scss');
 require('@/components/header/header.js')//引入header组件
 require('@/components/footer/footer.js')//引入footer组件
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';//引入Bootstrap CSS
 $("<div>这是jquery生成的</div>").appendTo("body");
